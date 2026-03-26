4acb6413fce32e4de246bc176da4a828107efa05
AlexxGutt <alex.gutt89@gmail.com>
2026-03-26 22:06:39 +0300

    API настроил, работаю дальше!

--- src/service/api/apiAddCourse.ts
+++ src/service/api/apiAddCourse.ts
@@ -2,10 +2,15 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 export const getAddCourse = (token: string, id: string) => {
-  return axios.post(BASE_URL + "/api/fitness/users/me/courses", id, {
-    headers: {
-      "Application-type": "",
-      "Authorization": `Bearer ${token}`,
+  console.log(`ID: ${id}`);
+  return axios.post(
+    BASE_URL + "/api/fitness/users/me/courses",
+    { courseId: id },
+    {
+      headers: {
+        "Content-Type": "",
+        "Authorization": `Bearer ${token}`,
+      },
     },
-  });
+  );
 };
