7dfd73b027565f478e1c1b04c3cc0e6466db07f2
AlexxGutt <alex.gutt89@gmail.com>
2026-04-01 08:53:19 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "skyfitness";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   reactStrictMode: false,
   allowedDevOrigins: ["192.168.0.108"],
+
+  output: "export",
+
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}` : "",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
